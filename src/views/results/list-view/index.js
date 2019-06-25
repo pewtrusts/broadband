2cb3fa392eb1c2c26d3462db64bc8366af5f6e53
John Osterman <josterman@pewtrusts.org>
2019-06-25 10:55:06 -0400

    Style list items

--- src/views/results/list-view/index.js
+++ src/views/results/list-view/index.js
@@ -25,7 +25,7 @@ export default class ListView extends Element {
             <div class="js-list-item ${s.listItem}" id="list-item-${cur.id}">
               <div class="${s.itemHeader} flex space-between"><p>${cur.state}</p><p>${cur.year !== 'Not specified' ? cur.year : 'no date'}</p></div>
               <h2 class="${s.itemHed}">${cur.name}${ this.model.names[cur.name + ' ' + cur.state] > 1 ? ' <span class="' + s.parens + '">(' + cur.topic + ')</span>' : ''}</h2>
-              <p>${cur.title.replace(/\.$/,'')}</p>
+              <p class="${s.itemTitle}">${cur.title.replace(/\.+ *$/,'')}</p>
               <div class="flex space-between">
                 <p><strong>Category:</strong> ${this.model.dictionary[cur.category]}</p>
                 <p><strong>Topic:</strong> ${cur.topic}${ cur.subtopic ? ' (' + cur.subtopic + ')' : ''}</p>
